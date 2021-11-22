968a8d88e51aa18fc3dadb4e78859e0ed841def7
Lucas de Matos <lucasmatos592@gmail.com>
2021-11-21 23:00:29 -0300

    Data Input

---
+++ data_input.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+
+	idade :=  0
+
+	fmt.Print("Informe sua idade: ")
+	fmt.Scanf("%v", &idade)
+
+	fmt.Printf("Sua idade é igual a: %v anos", idade)
+}
+
+
